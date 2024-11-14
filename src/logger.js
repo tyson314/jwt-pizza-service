58b9ba7e57e4a7c4c3397cfcd1eb2ddf7a02acc4
tyson314 <tysonpeterson4@gmail.com>
2024-11-14 13:37:16 -0700

    D9

---
+++ src/logger.js
@@ -0,0 +1,62 @@
+const config = require('./config.js');
+
+class Logger {
+  httpLogger = (req, res, next) => {
+    let send = res.send;
+    res.send = (resBody) => {
+      const logData = {
+        authorized: !!req.headers.authorization,
+        path: req.path,
+        method: req.method,
+        statusCode: res.statusCode,
+        reqBody: JSON.stringify(req.body),
+        resBody: JSON.stringify(resBody),
+      };
+      const level = this.statusToLogLevel(res.statusCode);
+      this.log(level, 'http', logData);
+      res.send = send;
+      return res.send(resBody);
+    };
+    next();
+  };
+
+  log(level, type, logData) {
+    const labels = { component: config.source, level: level, type: type };
+    const values = [this.nowString(), this.sanitize(logData)];
+    const logEvent = { streams: [{ stream: labels, values: [values] }] };
+
+    this.sendLogToGrafana(logEvent);
+  }
+
+
+  statusToLogLevel(statusCode) {
+    if (statusCode >= 500) return 'error';
+    if (statusCode >= 400) return 'warn';
+    return 'info';
+  }
+
+  nowString() {
+    return (Math.floor(Date.now()) * 1000000).toString();
+  }
+
+  sanitize(logData) {
+    logData = JSON.stringify(logData);
+    logData = logData.replace(/\bpassword\s*=\s*([^,)\s]*)/g, 'password=*****');
+    return logData.replace(/\\"password\\":\s*\\"[^"]*\\"/g, '\\"password\\": \\"*****\\"');
+  }
+
+  sendLogToGrafana(event) {
+    const body = JSON.stringify(event);
+    fetch(`${config.url}`, {
+      method: 'post',
+      body: body,
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${config.userId}:${config.apiKey}`,
+      },
+    }).then((res) => {
+      if (!res.ok) console.log('Failed to send log to Grafana');
+    });
+  }
+}
+module.exports = new Logger();
