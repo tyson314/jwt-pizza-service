f4de44ed40240df4eb7dd5f96ad39d2fb274e6dd
tyson314 <tysonpeterson4@gmail.com>
2024-11-21 22:25:53 -0700

    d10

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,4 +7,9 @@ export default [
   {languageOptions: { globals: globals.node }},
   { languageOptions: { globals: globals.jest } },
   pluginJs.configs.recommended,
+  {
+    ignores: [
+      "loadTests/"
+    ]
+  },
 ];
